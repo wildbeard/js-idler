082eaf59f10e8cfb4890d9518bb007627ca6aed4
Preston Haddock <quotablesarcasm@gmail.com>
2026-03-20 13:48:41 -0600

    Bumped ver

--- src/game/persistence.ts
+++ src/game/persistence.ts
@@ -10,7 +10,7 @@ import {
 } from './UpgradableEntity';
 import { updateAssistantJobs } from './assistants';
 
-const VERSION = '0.2.0';
+const VERSION = '0.1.19';
 
 export const startingState: GameState = {
   version: VERSION,
